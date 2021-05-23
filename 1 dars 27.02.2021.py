f6ccedca79a599db2ced34e13c2b5ed67f151fc1
Pahriddin-Abdikarimov <84650693+Pahriddin-Abdikarimov@users.noreply.github.com>
2021-05-23 10:44:44 +0600

    Add files via upload

---
+++ 1 dars 27.02.2021.py
@@ -0,0 +1,83 @@
+
+# gullar=['lola','Gullolam','tojihoroz']
+# gullar.sort()
+# print(gullar)
+
+
+# gullar=['lola','Gullolam','tojihoroz','dugonalar']
+# gullar.sort(reverse=True)
+# print(gullar)
+
+
+# dugonalar=['lola','Gullolam','tojihoroz','dugonalar']
+# print(dugonalar)
+# print("sorted() royhat:", sorted(dugonalar))
+# print("asl ro'yhad o'zgarmas:",dugonalar)
+# dugonalar_t=sorted(dugonalar)
+# print(dugonalar_t)
+
+
+# yillar=[2015,2018,2012,2017,2020]
+# print(yillar)
+# yillar.sort()
+# print(yillar)
+# print(sorted(yillar,reverse=True))
+
+
+# yillar=[2015,2018,2012,2017,2020]
+# print(yillar)
+# yillar.sort()
+# print(sorted(yillar))
+# print(sorted(yillar, reverse=True))
+# yillar_t=sorted(yillar)
+# print(yillar_t)
+
+
+# mevalar= ['olma', 'orik', 'shaftoli', 'uzum']
+# print("Elementlar soni",(mevalar))
+
+
+# juft_sonlar =list(range(2,21,2))   # 2 dan 20 cha juft sonlarni chiqaradi 
+# toq_sonlar =list(range(3,20,2))    # 3 dan 20 cha toq sonlarni chiqaradi 
+# print("juft_sonlar: ",juft_sonlar) #
+# print("toq_sonlar: ",toq_sonlar)
+# print(list(range(1,102,5)))       # 1 dan 102 cha sonlardan 5 sonlardan olib utiradi 
+
+# narhlar=[20325,22538,112112,200017,322020]
+# arzon= min(narhlar)
+# qimmat= max(narhlar)
+# jami=sum(narhlar)
+# print("arzon: ",arzon,".qimmat:",qimmat,".jami:",jami) 
+# # print(jami//len(narhlar))                 # len barchasini qushib ortacha qiymatini topadi
+
+# # hayvonlar=['it','mushuk','ot','sigir','qoy','echki']
+# # u_hayvonlar=hayvonlar[2:5]                 # hayvonlardan 2 va 5 hayvonlarni chiqartirib beradi
+# # print(u_hayvonlar)
+# # print(hayvonlar[2:4])                      # hayvonlardan 2 va 4 hayvonlarni chiqartirib beradi
+# print(hayvonlar[4])                        # hayvonlardan 4 hayvonni yani 'qoy' chiqartirib beradi
+# print(hayvonlar[:4])                      # hayvonlardan 1 dan 4 cha hayvonlarni chiqartirib beradi
+# print(hayvonlar[4:])              # hayvonlardan 4 chi hayvondan keyningi hayvonni chiqartirib beradi
+
+
+# sonlar=[3,4,5,6,7]                  #sonlar degan royhat yaratamiz
+# sonlar2=sonlar                   #sonlar2 degan royhatni sonlarga tenglaymiz
+# sonlar2.append(8)                #sonlar2 ga 6 sonini qoshamiz
+# sonlar2.append(9)                #sonlar2 ga 7 sonini qoshamiz
+# sonlar2.append(10)               #sonlar2 ga 8 sonini qoshamiz
+# sonlar.append(12)
+# sonlar.insert(4,14)
+# print("Bu sonlar ro'yhati: ",sonlar) 
+# print("Bu sonlar2 royhati: ",sonlar2) 
+
+# tomonlar= (20,30,55.2)
+# print(tomonlar)
+
+# oyinchilar=['mashina','Avtobus','ayiq','quyon']
+# print(oyinchilar[2])                # oyinchilardan 0 degani birinchi turganni chiqaradi
+# print(oyinchilar[-1])               # oyinchilardan -1 degani ohiridan boshlab chiqaradi
+# print("asl ro'yhad o'zgarmas:",oyinchilar) # oyinchilardan asl royhati  chiqaradi
+# oyinchilar_t=sorted(oyinchilar) # oyinchilarni t=sorted bosh harfdagini birinchi qolganlarini alfavit boyicha chiqartadi
+# print(oyinchilar_t)   # oyinchilarni t=sorted bosh harfdagini birinchi qolganlarini alfavit boyicha chiqartadi
+
+
+
