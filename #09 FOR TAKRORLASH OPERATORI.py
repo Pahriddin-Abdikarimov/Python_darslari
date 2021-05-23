a54037d5a0fbde2151ef57fd59cccb802e65e76f
Pahriddin-Abdikarimov <84650693+Pahriddin-Abdikarimov@users.noreply.github.com>
2021-05-23 11:25:38 +0600

    Create #09 FOR TAKRORLASH OPERATORI.py
    For operatori

---
+++ #09 FOR TAKRORLASH OPERATORI.py
@@ -0,0 +1,58 @@
+
+# # va ro'yxatdagi har bir ismga takrorlanuvchi xabar yozish
+# joralar = ['Sobir','Oybek','Hasan','Umid','Olim','farhod']
+# for joralar in joralar:
+#     print(f"Hurmatli, {joralar}. Sizni 7-mart kuni nahorga oshga taklif qilamiz")
+#     print("Hurmat bilan, Alimovlar oilasi")
+
+
+# # va ro'yxatdagi har bir ismga takrorlanuvchi xabar takrorlaydi
+# joralar = ['Sobir','Oybek','Hasan','Umid','Olim','farhod']
+# for joralar in joralar:
+#  print(f"Hurmatli, {joralar}. Sizni 7-mart kuni nahorga oshga taklif qilamiz")
+# print("Hurmat bilan, Alimovlar oilasi")  #ikinchi print bir marta takrorlaydi
+
+
+
+# sonlar = list(range(1,13))
+# for son in sonlar:
+#     print(f"{son} ning kubi {son**3} ga teng")
+
+
+# sonlar = list(range(1,13))
+# for son in sonlar:
+#     print(f"{son} ning kubi {son**3} ga teng")
+# print(son) # eng ohirgi sonni saqlab qoladi
+
+# sonlar = list(range(16)) # 0 dan 15 gacha sonlarni chiqarib beradi
+# sonlar_kvadrati=[]  # bo'sh ro'yhat yaratadi
+# for son in sonlar: # sonlardagi har bir  son uchun
+#     sonlar_kvadrati.append(son**2) # uni
+    
+# print(sonlar)
+# print(sonlar_kvadrati)           # eng ohirgi sonni saqlab qoladi
+
+
+# dostlar = [] # bosh royhat
+# print("5 ta eng yaqindostingiz kim?")
+# for k in range(6): # 0 dan 5 gacha sonlarni oladi
+#     dostlar.append(input(f"{k+1} dostingizni ismini kiriting:"))
+#     print(dostlar)    
+
+
+           #TOPSHIRIQ
+
+#1
+
+# ismlar = ['Sobir','Oybek','Hasan','Umid','Olim','Ahmad']
+# for ismr in ismlar:
+#     print(f"Salom, {ismlar}. sizni bizning uyga taklif qilaman")
+# print("Kod", len(ismlar),"marta takrorlandi")  #len funksiyasi ismlarni hisoblab chiqaradi
+
+
+
+
+
+
+
+
